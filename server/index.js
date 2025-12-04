5e3879a87aaa5fb64f43537e5c715f0a7c5fd330
Mahesh Jadhav <Maheshjadhav9359@gmail.com>
2025-12-04 13:32:02 +0530

    fix: update server index to include MongoDB connection and modify health check endpoint

--- server/index.js
+++ server/index.js
@@ -6,14 +6,21 @@ import cors from 'cors';
 dotenv.config();
 
 const app = express();
-app.use(cors());
 app.use(express.json());
+app.use(cors());
+
+const connectDB = async () => {
+    const conn = await mongoose.connect(process.env.MONGODB_URL);
+
+    if (conn) {
+        console.log('MongoDB connected successfully');
+    }
+}
 
 
-
-app.get('/health', (req, res) => {
+app.get('/', (req, res) => {
   res.json({
-    success: true,
+    status: "OK",
     message: 'Server is running smoothly!',
   });
 });
@@ -23,4 +30,5 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+  connectDB();
 });
