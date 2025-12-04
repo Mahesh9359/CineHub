05cb3bcea38290c278da5c398ad9122f7e1af8ba
Mahesh Jadhav <Maheshjadhav9359@gmail.com>
2025-12-04 14:06:19 +0530

    Fix: Updated connection string in try & catch block

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import mongoose from 'mongoose';
-import cors from 'cors';
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import cors from "cors";
 
 dotenv.config();
 
@@ -10,21 +10,23 @@ app.use(express.json());
 app.use(cors());
 
 const connectDB = async () => {
+  try {
     const conn = await mongoose.connect(process.env.MONGODB_URL);
 
     if (conn) {
-        console.log('MongoDB connected successfully');
+      console.log("MongoDB connected successfully");
     }
-}
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
+  }
+};
 
-
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.json({
     status: "OK",
-    message: 'Server is running smoothly!',
+    message: "Server is running smoothly!",
   });
 });
-
 
 const PORT = process.env.PORT || 5000;
 
