e044baa7577a661b133cdea7938a543b9d736af6
Mahesh Jadhav <Maheshjadhav9359@gmail.com>
2025-12-04 17:01:10 +0530

    feat: implement POST endpoint for adding new movies

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import cors from "cors";
+import Movie from "./models/Movies.js";
 
 dotenv.config();
 
@@ -27,6 +28,41 @@ app.get("/", (req, res) => {
     message: "Server is running smoothly!",
   });
 });
+
+app.post("/movies", async (req, res) => {
+  const {
+    title,
+    description,
+    images,
+    director,
+    year,
+    genre,
+    cast,
+    rating,
+    duration,
+  } = req.body;
+
+  const newMovie = new Movie({
+    title,
+    description,
+    images,
+    director,
+    year,
+    genre,
+    cast,
+    rating,
+    duration,
+    createdAt: new Date(),
+  })
+
+  const savedMovie = await newMovie.save();
+  res.json({
+    success: true,
+    message: "Movie data received",
+    data: req.body,
+  });
+});
+
 
 const PORT = process.env.PORT || 5000;
 
